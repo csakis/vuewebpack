0dbc0a9b98e89fa7daf28155174d14c49ff1b24b
Csaba Kiss <csakis@gmail.com>
2016-11-28 21:21:53 -0700

    first commit

---
+++ vue/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+ 
+  <div id="app">
+   <img src="../img/logo.png" alt="">
+<h1>this takes too long</h1>  
+   <special-div message="Hello"></special-div>
+   <special-div message="from"></special-div>
+   <special-div message="XPage"></special-div>
+  
+   <div> {{msg}}</div>
+</template>
+
+<script>
+import specDiv from './components/Div.vue'
+export default {
+  components: {
+    specialDiv: specDiv 
+  },
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Vue'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue/src/components/Div.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+<h3>{{ message }}</h3>
+</div>
+</template>
+<script>
+    export default {
+        props: ['message']       
+    }
+</script>
+<style>
+    
+</style>
